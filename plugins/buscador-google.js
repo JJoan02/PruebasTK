e711475ec632a6b2be563372d9024f21cd9b4d2b
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-08-21 10:37:48 -0500

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -1,66 +1,61 @@
-import {googleIt} from '@bochilteam/scraper';
+import { googleIt } from '@bochilteam/scraper';
 import google from 'google-it';
 import axios from 'axios';
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default;
-const text = args.join` `;
-if (!text) throw `${lenguajeGB['smsAvisoMG']()} ${mid.smsMalused} *${usedPrefix + command} Cat*`
-try {
-const url = 'https://google.com/search?q=' + encodeURIComponent(text);
-google({'query': text}).then(res => {
-let teks = `🔍 ${mid.buscador9} ${text}\n\n*${url}*\n\n`
-for (let g of res) {
-teks += `_${g.title}_\n_${g.link}_\n_${g.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`
-} 
-const ss = `https://image.thum.io/get/fullpage/${url}`
-conn.sendFile(m.chat, ss, 'error.png', teks, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: imagen4, sourceUrl: accountsgb }}})
-//m.reply(teks)
-})
-} catch {    
-handler.limit = 0
-}}
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^googlef?$/i
-handler.register = true
-handler.limit = 1
-export default handler
 
-import { googleIt } from '@bochilteam/scraper'
-let handler = async (m, { conn, command, args, usedPrefix }) => {
-const fetch = (await import('node-fetch')).default
-let full = /f$/i.test(command)
-let text = args.join` `
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}𝙀𝙎𝘾𝙍𝙄𝘽𝘼 𝙇𝙊 𝙌𝙐𝙀 𝙌𝙐𝙄𝙀𝙍𝙀 𝘽𝙐𝙎𝘾𝘼𝙍\n𝙀𝙅𝙀𝙈𝙋𝙇𝙊\n*${usedPrefix + command} Gata*\n\n𝙏𝙔𝙋𝙀 𝙒𝙃𝘼𝙏 𝙔𝙊𝙐 𝙒𝘼𝙉𝙏 𝙏𝙊 𝙎𝙀𝘼𝙍𝘾𝙃 𝙁𝙊𝙍\n𝙀𝙓𝘼𝙈𝙋𝙇𝙀\n*${usedPrefix + command} Cat*`, m)
-let pp = './media/menus/Menu1.jpg'
-let url = 'https://google.com/search?q=' + encodeURIComponent(text)
-let search = await googleIt(text)
-let msg = search.articles.map(({
-// header,
-title,
-url,
-description
-}) => {
-return `*${title}*\n_${url}_\n_${description}_\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈`
-}).join('\n\n')
+const THUM_IO_URL = 'https://image.thum.io/get/fullpage/';
+const GOOGLE_SEARCH_URL = 'https://google.com/search?q=';
+const MENSAJE_ERROR_INPUT = 'Por favor, escribe algo para buscar.';
+const MENSAJE_CARGA = '🔎 Buscando...';
+const MENSAJE_ERROR_BUSQUEDA = 'Hubo un error al realizar la búsqueda.';
 
-let info = `💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*
-  `.trim()
-conn.sendHydrated(m.chat, info, wm, pp, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙈𝙚𝙣𝙪 𝘽𝙪𝙨𝙦𝙪𝙚𝙙𝙖𝙨 | 𝙎𝙚𝙖𝙧𝙘𝙝𝙚𝙨 🔎', '#buscarmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,) 
-try {
-let ss = await (await fetch(global.API('nrtm', '/api/ssweb', { delay: 1000, url, full }))).arrayBuffer()
-if (/<!DOCTYPE html>/i.test(ss.toBuffer().toString())) throw ''
-await conn.sendFile(m.chat, ss, 'error.png', url + '\n\n' + msg, m)
-} catch (e) {
-m.reply(msg)
-}}
-handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
-handler.tags = ['internet']
-handler.command = /^googlef?$/i
-handler.exp = 40
-handler.exp = 3
-export default handler*/
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+  const fetch = (await import('node-fetch')).default;
+  const textoBusqueda = args.join` `.trim();
+  
+  if (!textoBusqueda) {
+    throw `${lenguajeGB['smsAvisoMG']()} ${MENSAJE_ERROR_INPUT}`;
+  }
+
+  m.reply(MENSAJE_CARGA);
+
+  try {
+    const urlBusqueda = GOOGLE_SEARCH_URL + encodeURIComponent(textoBusqueda);
+    const resultados = await google({ query: textoBusqueda });
+    
+    if (!resultados.length) {
+      throw new Error(MENSAJE_ERROR_BUSQUEDA);
+    }
+    
+    let textoResultados = `🔍 Resultados para: *${textoBusqueda}*\n\n*${urlBusqueda}*\n\n`;
+    resultados.forEach((res) => {
+      textoResultados += `_${res.title}_\n_${res.link}_\n_${res.snippet}_\n\n┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈\n\n`;
+    });
+
+    const screenshotUrl = `${THUM_IO_URL}${urlBusqueda}`;
+    await conn.sendFile(m.chat, screenshotUrl, 'resultados.png', textoResultados, fkontak, false, {
+      contextInfo: {
+        externalAdReply: {
+          mediaUrl: null,
+          mediaType: 1,
+          description: null,
+          title: gt,
+          body: '💻 𝑺𝒖𝒑𝒆𝒓 𝑨𝒅𝒎𝒊𝒏-𝑻𝑲 - 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑',
+          previewType: 0,
+          thumbnail: imagen4,
+          sourceUrl: accountsgb
+        }
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    m.reply(MENSAJE_ERROR_BUSQUEDA);
+  }
+};
+
+handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>');
+handler.tags = ['internet'];
+handler.command = /^googlef?$/i;
+handler.register = true;
+handler.limit = 1;
+
+export default handler;
