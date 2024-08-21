143413d25ef7d041d972f98da57cb9f1c6c0734b
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-08-21 01:31:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
-#!bin/bash
-GREEN='\033[0;32m'
-while :
-do
-echo ""
-    npm start
-    sleep 1
+#!/bin/bash
 
-done 
+# Definir el color verde para mensajes en la consola
+GREEN='\033[0;32m'
+
+# Bucle infinito para reiniciar la aplicación en caso de fallo
+while true; do
+  echo -e "${GREEN}Iniciando npm start...${RESET}"
+  
+  # Ejecutar el comando npm start
+  npm start
+  
+  # Esperar 1 segundo antes de reiniciar el bucle
+  sleep 1
+done
